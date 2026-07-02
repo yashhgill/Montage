c464ade027fde06b7b50324d7a93f22e6fe15afe
yashhgill <154743285+yashhgill@users.noreply.github.com>
2026-07-02 18:08:11 +0800

    Add react-router routes for /bookings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
+import BookingsPage from "@/pages/BookingsPage";
+import BookingSuccessPage from "@/pages/BookingSuccessPage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/bookings" element={<BookingsPage />} />
+        <Route path="/bookings/success" element={<BookingSuccessPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
