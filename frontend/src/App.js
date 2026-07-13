fdd74f8b58daf9da586ea0aa394e839a8c3d4e7e
yashhgill <154743285+yashhgill@users.noreply.github.com>
2026-07-14 03:15:26 +0800

    Add /admin/newsletter route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
 import BookingsPage from "@/pages/BookingsPage";
 import BookingSuccessPage from "@/pages/BookingSuccessPage";
+import AdminNewsletterPage from "@/pages/AdminNewsletterPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/bookings" element={<BookingsPage />} />
         <Route path="/bookings/success" element={<BookingSuccessPage />} />
+        <Route path="/admin/newsletter" element={<AdminNewsletterPage />} />
       </Routes>
     </BrowserRouter>
   );
